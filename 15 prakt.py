2c9614eb245b6cb5c66495d521fcdc0c692b44a7
Nikilot1989 <127592730+Nikilot1989@users.noreply.github.com>
2024-04-23 23:43:12 +0300

    Add files via upload

---
+++ 15 prakt.py
@@ -0,0 +1,23 @@
+"""def prov(item):
+    if item %2!=0:
+        return True
+    else:return False
+a=input().split(" ")
+b=[i for  i in a if prov(int(i))==True ]
+print(b)"""
+
+def sim(y):
+    for i in y:
+          return (y,len(y))
+       
+stuk=input().split(" ")
+h = sim(stuk)
+d = {city: length for city, length in h}
+print(d)
+
+
+
+
+
+
+
